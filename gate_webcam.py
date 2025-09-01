90d07e647a264d541fe2e60bbe84bf298bae7212
Athos <athosbds@gmail.com>
2025-09-01 16:13:37 -0300

    Update gate_webcam.py

--- gate_webcam.py
+++ gate_webcam.py
@@ -8,9 +8,9 @@ with open("moradores.json", "r", encoding="utf-8") as f:
     residents = json.load(f)["moradores"]
 
 
-for r in residents:
-    if not os.path.isfile(r["rosto"]):
-        print(f"⚠️ Imagem do morador {r['nome']} não encontrada: {r['rosto']}")
+for resident in residents:
+    if not os.path.isfile(resident["rosto"]):
+        print(f"Imagem do morador {resident['nome']} não encontrada: {resident['rosto']}")
 
 
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
@@ -19,11 +19,9 @@ print("Pressione 'q' para sair.")
 while True:
     ret, frame = cap.read()
     if not ret:
-        print("⚠️ Não foi possível capturar o frame da câmera.")
+        print("Não foi possível capturar o frame da câmera.")
         break
-
     cv2.imshow("Camera", frame)
-
     for resident in residents:
         try:
             if os.path.isfile(resident["rosto"]):
@@ -33,11 +31,11 @@ while True:
                     enforce_detection=False
                 )
                 if resultado["verified"] and resident["autorizado"]:
-                    print(f"✅ Acesso liberado para {resident['nome']}")
-                    print("🔓 Portão aberto (simulação).")
+                    print(f"Acesso liberado para {resident['nome']}")
+                    print("Portão aberto - Simulando")
                     break
         except Exception as e:
-            print(f"Erro na verificação do morador {resident['nome']}: {e}")
+            print(f"erro na verificação do morador {resident['nome']}: {e}")
 
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
