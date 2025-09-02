9f6b5a3c0bbbed2f47b8d197a8602b9058b4e4e3
Athos <athosbds@gmail.com>
2025-09-02 15:53:17 -0300

    Update gate_webcam.py

--- gate_webcam.py
+++ gate_webcam.py
@@ -2,7 +2,6 @@ import cv2
 import json
 import os
 from deepface import DeepFace
-import numpy as np
 
 with open("moradores.json", "r", encoding="utf-8") as f:
     residents = json.load(f)["moradores"]
@@ -13,29 +12,33 @@ for resident in residents:
         print(f"Imagem do morador {resident['nome']} não encontrada: {resident['rosto']}")
 
 
-cap = cv2.VideoCapture(2, cv2.CAP_DSHOW)
-print("Pressione 'q' para sair.")
-
+cap = cv2.VideoCapture(0)
+print("q para sair")
+frame_count = 0
 while True:
     ret, frame = cap.read()
     if not ret:
-        print("Não foi possível capturar o frame da câmera.")
+        print("captura de câmera negada")
         break
-    cv2.imshow("Camera", frame)
-    for resident in residents:
-        try:
-            if os.path.isfile(resident["rosto"]):
-                resultado = DeepFace.verify(
-                    frame,
+    frame_small = cv2.resize(frame, (320, 240))
+    cv2.imshow("Camera", frame_small)
+    frame_count += 1
+    if frame_count % 5 == 0:
+        for resident in residents:
+            try:
+                if os.path.isfile(resident["rosto"]):
+                    resultado = DeepFace.verify(
+                    frame_small,
                     resident["rosto"],
-                    enforce_detection=False
+                    enforce_detection=False,
+                    detector_backend='opencv'
                 )
                 if resultado["verified"] and resident["autorizado"]:
-                    print(f"Acesso liberado para {resident['nome']}")
+                    print(f"acesso liberado para {resident['nome']}")
                     print("Portão aberto - Simulando")
                     break
-        except Exception as e:
-            print(f"erro na verificação do morador {resident['nome']}: {e}")
+            except Exception as e:
+                print(f"erro na verificação do morador {resident['nome']}: {e}")
 
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
