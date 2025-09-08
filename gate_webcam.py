8a6468f09f8d76cf88e329469c7aee39cd1ee479
Athos <athosbds@gmail.com>
2025-09-08 14:02:51 -0300

    model hog, resolution and rectangle fixed

--- gate_webcam.py
+++ gate_webcam.py
@@ -40,7 +40,6 @@ while True:
     ret, frame = cap.read()
     if not ret:
         break
-
     frame_count += 1
 
 
@@ -51,16 +50,16 @@ while True:
         continue
 
  
-    small_frame = cv2.resize(frame, (120, 90))
+    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
     rgb_small_frame = cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)
 
 
-    face_locations = face_recognition.face_locations(rgb_small_frame, model="cnn")
+    face_locations = face_recognition.face_locations(rgb_small_frame, model="hog")
     face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
     for face_encoding, face_location in zip(face_encodings, face_locations):
         matches = face_recognition.compare_faces(
-            known_face_encodings, face_encoding, tolerance=0.65
+            known_face_encodings, face_encoding, tolerance=0.4
         )
         name = "Desconhecido"
         authorized = False
@@ -77,7 +76,7 @@ while True:
         else:
             print("⚠️ Rosto desconhecido detectado")
 
-        top, right, bottom, left = [v * int(frame.shape[0] / small_frame.shape[0]) for v in face_location]
+        top, right, bottom, left = [v * 4 for v in face_location]
         color = (0, 255, 0) if authorized else (0, 0, 255)
         cv2.rectangle(frame, (left, top), (right, bottom), color, 2)
         cv2.putText(frame, name, (left, top - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, color, 2)
